78d383aad6b21467904cc6deab7c06974ace9183
lilyH <hlily871108@gmail.com>
2014-08-30 11:20:05 +0800

    增加台湾图片展示的部分
    增加台湾图片展示的部分

---
+++ ontheway/com/mobileGallery.js
@@ -0,0 +1,240 @@
+/**
+ * 基于zepto的mobile版本gallery
+ * 依赖zepto.js 以及zepto-touch.js
+ * author lilyh
+ * 2014-07-17
+ */
+(function($, window, document, undefined) {
+    var G = null,
+        element = null,
+        elementMask = null;
+    var Config = {
+       gHTML : [
+            '<div class="hd"></div>',
+            '<div class="bd">',
+                '<div id="gallery_list"></div>',
+            '</div>',
+            '<div class="ft"></div>'
+        ].join(''),
+        gList : {},
+        gImgParentList:{},
+        gImgList : {},
+        gImgLen : 0,
+        gImgListPreBtn : null,
+        gImgListNextBtn : null,
+        pContainer : null,
+        pCurImgUrl : '',
+        pPrevImgUrl : '',
+        pNextImgUrl :''
+    };
+    
+    var Gallery = function(settings){
+        var $settings = setSettings(settings);
+        
+        function closeGallary(){
+
+            removeEventPhone();
+            element.hide();
+            elementMask.hide();
+            Config.gList.html('');
+        }
+        
+        /**手机版本*/
+        function initEventPhone(){
+           
+            $('#gallery_list').on('swipeRight', previewPrev);
+            $('#gallery_list').on('swipeLeft', previewNext);
+            $('#gallery .bd').on('tap',closeGallary);
+        }
+        function removeEventPhone(){
+            $('#gallery_list').unbind('swipeRight', previewPrev);
+            $('#gallery_list').unbind('swipeLeft', previewNext);
+            $('#gallery .bd').unbind('tap',closeGallary);
+        }
+
+        /**
+        *   向左滑动或者点击右箭头时，查看next图片
+        */
+        function previewNext() {
+           
+            var curIndex = $settings.currentIndex,
+                imgList = $settings.imgInfo,
+                imgLen = Config.gImgLen;
+              
+           
+            if(!Config.pNextImgUrl) {
+                return;
+            } 
+            if($settings.currentIndex<imgLen-1) {
+                $settings.currentIndex ++;
+                Config.pCurImgUrl = imgList[$settings.currentIndex].url;
+                Config.pPrevImgUrl = $settings.currentIndex?imgList[$settings.currentIndex-1].url: '';
+                Config.pNextImgUrl = ($settings.currentIndex<imgLen-1)?imgList[$settings.currentIndex+1].url:'';
+            }
+            var h = $('body').height(),
+                w = $('body').width();  
+            var imgLis = Config.pContainer.find('li');
+            var newLi = $('<li><img /></li>');
+             $(imgLis[2]).css('-webkit-transform', 'translateX(0px)').css('-webkit-transition-duration', '.35s');
+             $(imgLis[1]).css('-webkit-transform', 'translateX(-'+w+'px)').css('-webkit-transition-duration', '.35s');
+            
+            $(imgLis[0]).remove();
+            newLi.appendTo(Config.pContainer).css('-webkit-transform', 'translateX('+w+'px)').css('-webkit-transition-duration', '.35s');
+          
+            setImgUrl(1,[Config.pNextImgUrl],2);
+
+        }
+
+    
+        function previewPrev() {    
+            
+            var curIndex = $settings.currentIndex,
+                imgList = $settings.imgInfo,
+                imgLen = Config.gImgLen;
+            if(!Config.pPrevImgUrl) {
+                return;
+            } 
+            if($settings.currentIndex) {
+                $settings.currentIndex --;
+                Config.pCurImgUrl = imgList[$settings.currentIndex].url;
+                Config.pPrevImgUrl = $settings.currentIndex?imgList[$settings.currentIndex-1].url: '';
+                Config.pNextImgUrl = ($settings.currentIndex<imgLen-1)?imgList[$settings.currentIndex+1].url:'';
+            }
+            var h = $('body').height(),
+                w = $('body').width();      
+            var imgLis = Config.pContainer.find('li');
+            var newLi = $('<li><img /></li>');
+           
+            $(imgLis[0]).css('-webkit-transform', 'translateX(0px)').css('-webkit-transition-duration', '.35s');
+            $(imgLis[1]).css('-webkit-transform', 'translateX('+w+'px)').css('-webkit-transition-duration', '.35s');
+
+            $(imgLis[2]).remove();
+            
+            newLi.prependTo(Config.pContainer).css('-webkit-transform', 'translateX(-'+w+'px)').css('-webkit-transition-duration', '.35s');
+            setImgUrl(1,[Config.pPrevImgUrl],0);
+
+        }
+
+        function setImgUrl(len,urls,index){
+            var len = len,
+                url = urls;
+            //len :1 , index:2
+            for(var i=0 ;i<(len); i++){
+                if(len == 1){
+                    var j = index;
+                }else{
+                    var j = i;
+                }
+                $('#gallery_p_list img')[j].src = url[i];
+                $('#gallery_p_list img')[j].onload = function(){
+                    var item = this;
+                    $(item).parent('li').css('background-image', 'none');
+                };
+               
+            }
+        }
+        function imgInitPhone(){
+            var ctn = Config.gList,
+                curIndex = parseInt($settings.currentIndex,10),
+                urls = $settings.imgInfo;
+
+            Config.pPrevImgUrl = curIndex > 0 ? urls[curIndex-1].url : '';
+            Config.pNextImgUrl = curIndex < (Config.gImgLen-1) ? urls[curIndex+1].url : '';
+            Config.pCurImgUrl = urls[curIndex].url;
+            var prevImgUrl = Config.pPrevImgUrl,
+                nextImgUrl = Config.pNextImgUrl,
+                curImgUrl = Config.pCurImgUrl;
+            var w = $('body').width(),
+                h = $('body').height(); 
+            var html = ['<ul id="gallery_p_list">',
+                        '<li style="-webkit-transform : translateX(-'+w+'px)"></li>',
+                        '<li style="-webkit-transform : translateX(0px)"></li>',
+                        '<li style="-webkit-transform : translateX('+w+'px)"></li>',
+                     '</ul>'];
+            ctn.html(html.join(''));
+            Config.pContainer = $('#gallery_p_list');
+            
+            
+            var imgLis = Config.pContainer.find('li');   
+            $(imgLis[0]).html(prevImgUrl?('<img src="'+prevImgUrl+'" data-index="'+ (curIndex-1) +'" />'):'');
+            $(imgLis[1]).html('<img src="'+curImgUrl+'" data-index="'+ curIndex +'" />');
+            $(imgLis[2]).html(nextImgUrl?('<img src="'+nextImgUrl+'" data-index="'+ (curIndex+1) +'" />'):'');
+
+            if(prevImgUrl && nextImgUrl){//前后都有
+                setImgUrl(3,[prevImgUrl,curImgUrl,nextImgUrl],0);
+            }else{
+                if(prevImgUrl){//前有，后面没有
+                    setImgUrl(2,[prevImgUrl,curImgUrl],0);
+                }else if(nextImgUrl){//后有，前面没有
+                    setImgUrl(2,[curImgUrl,nextImgUrl],1);
+                }else{//前后，都没有
+                    setImgUrl(1,[curImgUrl],0);//这个时候index要传0
+                }
+            }
+
+        }
+        function setSettings(settings){
+            $settings = settings;
+            return $settings;
+        }
+        return {
+          
+            _imgInitPhone : imgInitPhone,
+            _initEventPhone : initEventPhone,
+            _setSettings : setSettings,
+            previewPrev : previewPrev,
+            previewNext : previewNext,
+            closeGallary : closeGallary
+        }
+    };
+    $.fn.gallery = function(options) {
+        //var element = this;
+        
+       
+        var settings = $.extend(true,{
+            imgInfo : [],
+            currentIndex : 0,
+            container : document.body
+        },options);
+        
+        var init = function(){
+
+            if(!G){
+                var galleryEl = document.createElement("div"),
+                    maskEl = document.createElement("div");
+                galleryEl.id = 'gallery';
+                maskEl.id = 'gallery-mask';
+              
+                $(settings.container).append(galleryEl);
+                $(settings.container).append(maskEl);
+                element = $('#gallery');
+                elementMask = $('#gallery-mask');
+                elementMask.css('height',$(settings.container).height()+'px');
+
+                G = new Gallery(settings);
+                $(element).html(Config.gHTML);
+                Config.gList = $('#gallery_list');
+                Config.gImgLen = settings.imgInfo.length;
+                Config.gImgListPreBtn = $('#gallery .pre_Btn');
+                Config.gImgListNextBtn = $('#gallery .next_Btn');
+
+            }else{
+                G._setSettings(settings);
+                element.show();
+                elementMask.show();
+            }
+
+            G._imgInitPhone();
+            G._initEventPhone();
+            return G;
+            
+        };
+        var MyGallery = init();
+     
+        return {
+            setPrev : MyGallery.previewPrev,
+            setNext : MyGallery.previewNext,
+            close : MyGallery.closeGallary
+        }
+    };
+})(Zepto, window, document);
--- ontheway/way.js
+++ ontheway/way.js
@@ -22,57 +22,104 @@ function XIAN(){
     XIAN.prototype = this; 
     instance = new WAY();  
     instance.constructor = XIAN; 
-    //其他初始化 
+    /**绘制家庭关系图*/
+    instance.ConnectGraph = function(){
+        var width = $(window).width()-20,height=$(window).height()-50;
+        var svg = d3.select(".page2-content").append("svg")
+                   .attr("width", width)
+                   .attr("height", height)
+                   .attr("class","xian");
+        var color = d3.scale.category20();
+        var force = d3.layout.force()
+            .charge(-80)
+            .linkDistance(10)
+            .size([width-10, height-10]);
+
+        d3.json("cp.json", function(error, graph) {
+          force
+              .nodes(graph.nodes)
+              .links(graph.links)
+              .start();
+
+          var link = svg.selectAll(".link")
+              .data(graph.links)
+              .enter().append("line")
+              .attr("class", "link")
+              .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+          var node = svg.selectAll(".node")
+              .data(graph.nodes)
+              .enter().append("circle")
+              .attr("class", "node")
+              .attr("r", 5)
+              .style("fill", function(d) { return color(d.group); })
+              .call(force.drag);
+
+          node.append("title")
+              .text(function(d) { return d.name; });
+          
+          force.on("tick", function() {
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node.attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+          });
+        });
+    };
+    /**覆盖start事件**/
     instance.start = function(){
         if(!WAYCON.init){
             WAYCON.init = true;
             instance.page2MainEl.html('');
-            var width = $(window).width()-20,height=$(window).height()-50;
-            var svg = d3.select(".page2-content").append("svg")
-                       .attr("width", width)
-                       .attr("height", height)
-                       .attr("class","xian");
-            var color = d3.scale.category20();
-            var force = d3.layout.force()
-                .charge(-80)
-                .linkDistance(10)
-                .size([width-10, height-10]);
-
-            d3.json("cp.json", function(error, graph) {
-              force
-                  .nodes(graph.nodes)
-                  .links(graph.links)
-                  .start();
-
-              var link = svg.selectAll(".link")
-                  .data(graph.links)
-                  .enter().append("line")
-                  .attr("class", "link")
-                  .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-              var node = svg.selectAll(".node")
-                  .data(graph.nodes)
-                  .enter().append("circle")
-                  .attr("class", "node")
-                  .attr("r", 5)
-                  .style("fill", function(d) { return color(d.group); })
-                  .call(force.drag);
-
-              node.append("title")
-                  .text(function(d) { return d.name; });
-              
-              force.on("tick", function() {
-                link.attr("x1", function(d) { return d.source.x; })
-                    .attr("y1", function(d) { return d.source.y; })
-                    .attr("x2", function(d) { return d.target.x; })
-                    .attr("y2", function(d) { return d.target.y; });
-
-                node.attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
-              });
-            });
+            this.ConnectGraph();
         }
-        
     }
     return instance; 
+}
+/**台湾子类*/
+function TAIWAN(){
+    var instance = this; 
+    TAIWAN = function(){ 
+        return instance; 
+    } 
+    TAIWAN.prototype = this; 
+    instance = new WAY();  
+    instance.constructor = TAIWAN; 
+
+    var Images = [
+      {url : 'http://p6.qhimg.com/t019875709696b69d86.jpg'},
+      {url : 'http://p4.qhimg.com/t01174ebc4950fa217d.jpg'},
+      {url : 'http://p6.qhimg.com/t0133499eee6e798d75.jpg'},
+      {url : 'http://p4.qhimg.com/t01e7d38f5ad6688577.jpg'},
+      {url : 'http://p1.qhimg.com/t01c96ad218a4b9eda5.jpg'},
+      {url : 'http://p7.qhimg.com/t01c9c9f98e8a255c2d.jpg'},
+      {url : 'http://p2.qhimg.com/t01032f593431942074.jpg'},
+      {url : 'http://p7.qhimg.com/t01d78d7765acd215cc.jpg'},
+      {url : 'http://p6.qhimg.com/t01b1ae53c9076a5a3b.jpg'},
+      {url : 'http://p8.qhimg.com/t0195691effa60705f3.jpg'},
+      {url : 'http://p2.qhimg.com/t0108a9ea3b517998ed.jpg'}
+    ];
+    /*大图预览**/
+    var mainHtml = '<div class="taiwan-content"><ul>';
+    for(var i=0; i<Images.length; i++){
+        mainHtml += '<li data-index="'+i+'"><img src="'+Images[i].url+'" /></li>';
+    }
+    mainHtml += '</ul></div>';
+    instance.start = function(){
+        if(!TAIWAN.init){
+            TAIWAN.init = true;
+            instance.page2MainEl.html(mainHtml);
+            $('.taiwan-content li').on("click",function(){
+               var item = $(this).data('index');
+               $('#gallery').gallery({
+                  imgInfo : Images,
+                  currentIndex : item
+               });
+            });
+        }
+    }
+    return instance;
 }
